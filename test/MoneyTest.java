807258f0116fbfe1e9f821414a1df9fdf6b8c872
Katsunori Kanda <potix2@gmail.com>
2012-04-14 17:07:54 +0900

    Finished chapter3

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -2,6 +2,8 @@ import Money.Dollar;
 import org.junit.*;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 public class MoneyTest {
     @Test
@@ -11,5 +13,11 @@ public class MoneyTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
