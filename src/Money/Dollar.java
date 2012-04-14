f8593c148b4059aceef3fa29465a77f635fd4e0f
Katsunori Kanda <potix2@gmail.com>
2012-04-14 17:18:32 +0900

    Finished chapter6

--- src/Money/Dollar.java
+++ src/Money/Dollar.java
@@ -1,7 +1,6 @@
 package Money;
 
-public class Dollar {
-    private int amount;
+public class Dollar extends Money {
     public Dollar(int amount) {
         this.amount = amount;
     }
@@ -10,8 +9,4 @@ public class Dollar {
         return new Dollar(amount * multiplier);
     }
 
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
-    }
 }
--- src/Money/Franc.java
+++ src/Money/Franc.java
@@ -1,17 +1,11 @@
 package Money;
 
-public class Franc {
-    private int amount;
+public class Franc extends Money {
     public Franc(int amount) {
         this.amount = amount;
     }
 
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
     }
 }
