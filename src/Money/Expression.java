1e46bb6fa9a8e5d3420c4a03cb46e9035f2d6424
Katsunori Kanda <potix2@gmail.com>
2012-04-15 15:35:00 +0900

    Finished chapter16

--- src/Money/Expression.java
+++ src/Money/Expression.java
@@ -3,4 +3,5 @@ package Money;
 public interface Expression {
     public Money reduce(Bank bank, String to);
     public Expression plus(Expression addend);
+    public Expression times(int multiplier);
 }
