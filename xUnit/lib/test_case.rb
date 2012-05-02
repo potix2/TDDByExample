f7144967e5fee1b6b7dfd82c31c7d4be67ab6e0b
Katsunori Kanda <potix2@gmail.com>
2012-05-02 23:44:42 +0900

    finished chapter19

--- xUnit/lib/test_case.rb
+++ xUnit/lib/test_case.rb
@@ -5,8 +5,15 @@ class TestCase
     @name = name
   end
 
+  def setUp
+  end
+
   def run
+    setUp
     send(@name)
   end
 
+  def assert(predicate)
+    raise "assert: " << caller.join("\n") unless predicate
+  end
 end
--- xUnit/test/test_case_test.rb
+++ xUnit/test/test_case_test.rb
@@ -1,10 +1,19 @@
 require 'was_run'
 class TestCaseTest < TestCase
+  attr_accessor :test
+  def setUp
+    @test = WasRun.new("testMethod")
+  end
+
+  def testSetUp
+    @test.run()
+    assert @test.wasSetUp
+  end
+
   def testRunning
-    test = WasRun.new("testMethod")
-    raise "assert" if test.wasRun
-    test.run()
-    raise "assert" unless test.wasRun
+    @test.run()
+    assert @test.wasRun
   end
 end
 TestCaseTest.new("testRunning").run()
+TestCaseTest.new("testSetUp").run()
